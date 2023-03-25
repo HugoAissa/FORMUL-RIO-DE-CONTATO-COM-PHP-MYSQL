24c40e6869d7832fd12cf9608d46691d88333046
HugoAissa <112508131+HugoAissa@users.noreply.github.com>
2023-03-24 21:46:13 -0300

    Add files via upload

---
+++ database/banco de dados.sql
@@ -0,0 +1,7 @@
+create database formbd;
+use formbd;
+show databases;
+
+create table contato (id int auto_increment primary key, nome varchar(100), email varchar (100), msg text); 
+
+select * from contato; 
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+$mensagem = "Preencha os dados do formulário";
+$nome = "";
+$email = "";
+$msg = "";
+
+if (isset($_POST["nome"], $_POST["email"], $_POST["msg"])){    
+    $nome = filter_input (INPUT_POST, "nome", FILTER_SANITIZE_STRING);
+    $email = filter_input (INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $msg = filter_input (INPUT_POST, "msg", FILTER_SANITIZE_STRING);
+
+    if(!$nome || !$email || !$msg){
+        $mensagem = "Dados inválidos";
+
+    }else{
+        $conexao = new PDO ("mysql:host=localhost;dbname=formbd", "root", "123");
+
+        $stm = $conexao -> prepare ('INSERT INTO contato(nome,email,msg) VALUES (:nome,:email,:msg)');
+        $stm ->BindParam('nome', $nome);
+        $stm ->BindParam('email', $email);
+        $stm ->BindParam('msg', $msg);
+        $stm ->execute();
+
+        $mensagem = "Mensagem Enviada com Sucesso!";
+
+    }
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <main>
+        <form method="POST">
+            <label>Nome</label>
+            <input type="text" name='nome' require />
+
+            <label>Email</label>
+            <input type="email" name='email' require />
+
+            
+            <label>Mensagem</label>
+            <textarea name='msg'></textarea>
+
+            <button type="submit">Enviar</button>
+        </form>
+        <div class="mensagem">
+            <?=$mensagem?>
+        </div>
+    </main>
+    
+</body>
+</html>
